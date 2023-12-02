6392225a8f7522ff537955e1ce92b903dc90d6bf
Yuvraj Chettri <chettriyuvraj@gmail.com>
2023-12-02 23:09:42 +0530

    1. feat: take input from cli and conduct story 1 game

--- Basics/GameOfPig/main.go
+++ Basics/GameOfPig/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+const rounds = 10
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("--usage: ./GameOfPig [holdcount1] [holdcount2]")
+		return
+	}
+
+	holdcount1, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	holdcount2, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	player1 := Player{strategy: getHoldAtValueStrategy(holdcount1)}
+	player2 := Player{strategy: getHoldAtValueStrategy(holdcount2)}
+	conductor := Conductor{player1: player1, player2: player2}
+	conductor.Conduct(rounds)
+}
+
+func RollDie() int {
+	return rand.Intn(6) + 1
+}
