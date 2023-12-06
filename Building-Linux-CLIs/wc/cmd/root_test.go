0fdf62eb8ddaa8b748594b438ca232c5f684745a
Yuvraj Chettri <chettriyuvraj@gmail.com>
2023-12-06 12:35:35 +0530

    1. feat: create distinction betwen error tests and and success tests

--- Building-Linux-CLIs/wc/cmd/root_test.go
+++ Building-Linux-CLIs/wc/cmd/root_test.go
@@ -11,50 +11,66 @@ func TestLFlag(t *testing.T) {
 		name string
 		args []string
 		want string
+		err  string
 	}{
 		{
 			name: "Error when file does not exist",
-			args: []string{"-l", "testfile"},
-			want: "wc: testfile: open: no such file or directory",
+			args: []string{"testfile"},
+			want: "",
+			err:  "wc: testfile: open: no such file or directory",
 		},
 		{
 			name: "Error when file is a directory",
-			args: []string{"-l", "testdata"},
-			want: "wc: testdata: read: is a directory",
+			args: []string{"testdata"},
+			want: "",
+			err:  "wc: testdata: read: is a directory",
 		},
 		{
 			name: "Successful count of lines in file",
-			args: []string{"-l", "testdata/test.txt"},
+			args: []string{"testdata/test.txt"},
 			want: "       3 testdata/test.txt",
+			err:  "",
 		},
 		{
 			name: "Successful count of lines in stdin",
-			args: []string{"-l"},
+			args: []string{},
 			want: "       3",
+			err:  "",
 		},
 	}
 
+	flags := []string{"-l"} //, "-w"} //, "-m", "-lwm", "-mwl"}
 	cmd := rootCmd
+
 	for _, test := range tc {
-		var b strings.Builder
+		for _, flag := range flags {
+			test.args = append([]string{flag}, test.args...)
+			var b strings.Builder
 
-		/* Special case: mocking stdin as a file if no args provided */
-		if len(test.args) < 2 {
-			stdin := os.Stdin
-			f, _ := os.Open("testdata/test.txt")
-			os.Stdin = f
-			defer func() {
-				os.Stdin = stdin
-			}()
-		}
+			/* Special case: mocking stdin as a file if no args provided */
+			if len(test.args) < 2 {
+				stdin := os.Stdin
+				f, _ := os.Open("testdata/test.txt")
+				os.Stdin = f
+				defer func() {
+					os.Stdin = stdin
+				}()
+			}
 
-		cmd.SetArgs(test.args)
-		cmd.SetOut(&b)
-		cmd.Execute()
+			cmd.SetArgs(test.args)
+			cmd.SetOut(&b)
+			cmd.Execute()
 
-		got := b.String()
-		if got != test.want {
-			t.Errorf("got %q, wanted %q", got, test.want)
+			got := b.String()
+			if test.err != "" {
+				if got != test.err {
+					t.Errorf("error testing: got %q, wanted %q", got, test.want)
+				}
+				return
+			}
+			if got != test.want {
+				t.Errorf("got %q, wanted %q", got, test.want)
+			}
 		}
 
 	}
