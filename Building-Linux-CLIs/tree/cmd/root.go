07cd02d0af641f82c01fe968f829ecd6aecee0fc
Yuvraj Chettri <chettriyuvraj@gmail.com>
2023-12-08 17:03:11 +0530

    1. feat: add core tree printing fuction

--- Building-Linux-CLIs/tree/cmd/root.go
+++ Building-Linux-CLIs/tree/cmd/root.go
@@ -7,6 +7,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const defaultbasepath = "."
+
 var rootCmd = NewRootCmd()
 
 func NewRootCmd() *cobra.Command {
@@ -15,10 +17,30 @@ func NewRootCmd() *cobra.Command {
 		Short: "List directory and files in a tree-like view",
 		Long:  "A command-utility which lists directories and files recursively in a tree-like view for the user.",
 		Run: func(cmd *cobra.Command, args []string) {
-			dir, _ := os.ReadDir("./")
-			for _, x := range dir {
-				fmt.Fprintf(cmd.OutOrStdout(), x.Name())
+			if len(args) == 0 {
+				args = append(args, defaultbasepath)
 			}
+
+			fcount, dcount := 0, 1
+			for _, path := range args {
+				fmt.Fprintf(cmd.OutOrStdout(), "%s", path)
+				fc, dc, err := tree(cmd, path, []bool{})
+				if err != nil {
+					fmt.Fprintf(cmd.OutOrStdout(), "%s", err.Error())
+				}
+				fcount += fc
+				dcount += dc
+			}
+
+			dirStr := "directory"
+			if dcount > 1 {
+				dirStr = "directories"
+			}
+			fileStr := "file"
+			if fcount > 1 {
+				fileStr = "files"
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), "\n\n%d %s, %d %s\n", dcount, dirStr, fcount, fileStr)
 		},
 	}
 }
@@ -29,4 +51,52 @@ func init() {
 
 func Execute() {
 	rootCmd.Execute()
+}
+
+func tree(cmd *cobra.Command, path string, isAncestorLastDir []bool) (fcount int, dcount int, err error) {
+	direntries, err := os.ReadDir(path)
+	if err != nil {
+		return -1, -1, err
+	}
+
+	for i, d := range direntries {
+		isLastElem := i == len(direntries)-1
+		printDirEntry(cmd, d, isLastElem, isAncestorLastDir)
+
+		if !d.IsDir() {
+			fcount += 1
+			continue
+		}
+
+		fc, dc, err := tree(cmd, fmt.Sprintf("%s/%s", path, d.Name()), append(isAncestorLastDir, isLastElem))
+		if err != nil {
+			return -1, -1, err
+		}
+		fcount += fc
+		dcount += dc + 1
+	}
+
+	return fcount, dcount, nil
+}
+
+/***** Helpers *****/
+
+func printDirEntry(cmd *cobra.Command, d os.DirEntry, isLastElem bool, isAncestorLastDir []bool) {
+	fmt.Fprintf(cmd.OutOrStdout(), "\n")
+
+	for _, b := range isAncestorLastDir {
+		if b {
+			fmt.Fprintf(cmd.OutOrStdout(), " ")
+		} else {
+			fmt.Fprintf(cmd.OutOrStdout(), "%s", vline)
+		}
+		fmt.Fprintf(cmd.OutOrStdout(), "   ")
+	}
+
+	if isLastElem {
+		fmt.Fprintf(cmd.OutOrStdout(), "%s%s%s %s", vshafthalf, hline, hline, d.Name())
+		return
+	}
+
+	fmt.Fprintf(cmd.OutOrStdout(), "%s%s%s %s", vshaft, hline, hline, d.Name())
 }
