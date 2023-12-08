a729584d8a80b9d95be7ce591a7c1a6123aed271
Yuvraj Chettri <chettriyuvraj@gmail.com>
2023-12-08 17:01:37 +0530

    1. test: change test soln to reflect dir structure

--- Building-Linux-CLIs/tree/cmd/root_test.go
+++ Building-Linux-CLIs/tree/cmd/root_test.go
@@ -12,21 +12,45 @@ func TestTreeBasic(t *testing.T) {
 		want string
 	}{
 		{
-			name: "Happy case",
-			args: []string{},
+			name: "Happy case current directory",
+			args: []string{"."},
 			want: `.
-			├── cmd
-			│   ├── root.go
-			│   └── root_test.go
-			├── go.mod
-			├── go.sum
-			├── main.go
-			└── testdir
-				├── testdirinner
-				├── testfile1.txt
-				└── testfile2.txt
-			
-			4 directories, 7 files`,
+├── chars.go
+├── root.go
+├── root_test.go
+└── testdata
+    ├── emptydir
+    └── testdir
+        ├── testdirinner
+        │   └── testdirinner.txt
+        ├── testfile1.txt
+        └── testfile2.txt
+
+5 directories, 6 files
+`,
+		},
+		{
+			name: "Happy case parent directory",
+			args: []string{"../"},
+			want: `../
+├── cmd
+│   ├── chars.go
+│   ├── root.go
+│   ├── root_test.go
+│   └── testdata
+│       ├── emptydir
+│       └── testdir
+│           ├── testdirinner
+│           │   └── testdirinner.txt
+│           ├── testfile1.txt
+│           └── testfile2.txt
+├── go.mod
+├── go.sum
+├── main.go
+└── notes.txt
+
+6 directories, 10 files
+`,
 		},
 	}
 
@@ -40,7 +64,7 @@ func TestTreeBasic(t *testing.T) {
 
 		got := b.String()
 		if got != test.want {
-			t.Errorf("\n\nTest: %s\nArgs: %s\nWant: %s\nGot: %s", test.name, test.args, test.want, got)
+			t.Errorf("\nTest: %s\nArgs: %s\nWant: %s\nGot: %s\n\n", test.name, test.args, test.want, got)
 		}
 	}
 }
